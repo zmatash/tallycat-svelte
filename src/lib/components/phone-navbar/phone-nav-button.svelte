b2d4d33c85306ca0d9fafe2bc67ab0dade72ab78
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-27 18:59:09 +0000

    new navbar component for mobile layout.

---
+++ src/lib/components/phone-navbar/phone-nav-button.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { page } from "$app/state";
+	import type { SpriteKey } from "$lib/common/generated/icons";
+	import getSprite from "$lib/utility/sprite-helpers";
+
+	interface Props {
+		goto: string;
+		sprite: SpriteKey;
+		label: string;
+
+		isEnabled?: boolean;
+	}
+
+	let { isEnabled = true, ...props }: Props = $props();
+
+	const onClick = () => {
+		if (!isEnabled) return;
+		goto(props.goto);
+	};
+
+	let isActive = $derived(page.route.id === props.goto);
+</script>
+
+<button
+	class={["fill-parent col-centred", { disabled: !isEnabled, selected: isActive && isEnabled }]}
+	type="button"
+	aria-label={`Go to ${props.goto}`}
+	onclick={onClick}
+>
+	<svg>
+		<title>{props.sprite}</title>
+		<use href={isEnabled ? getSprite(props.sprite) : getSprite("circle-chevron")} />
+	</svg>
+	{props.label}
+</button>
+
+<style>
+	button.disabled {
+		color: var(--overlay0);
+	}
+
+	button.selected {
+		font-weight: 600;
+		color: var(--mauve);
+	}
+
+	button {
+		justify-content: space-evenly;
+		color: var(--subtext0);
+	}
+
+	svg {
+		color: inherit;
+	}
+</style>
---
+++ src/lib/components/phone-navbar/phone-navbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { collectionStore } from "$lib/stores/collection-store.svelte";
+	import { profileStore } from "$lib/stores/profile-store.svelte";
+	import PhoneNavButton from "./phone-nav-button.svelte";
+
+	let activeCollectionProps = $derived.by(() => {
+		if (!profileStore.profile())
+			return {
+				label: "Disabled",
+				isEnabled: false
+			};
+		const activeId = profileStore.profile()?.activeCollection;
+		if (!activeId)
+			return {
+				label: "Disabled",
+				isEnabled: false
+			};
+		const collection = collectionStore.collections().find((c) => c.id === activeId);
+		return {
+			label: collection?.name ?? "Disabled",
+			isEnabled: collection !== undefined
+		};
+	});
+</script>
+
+<nav class="row-centred just-evenly">
+	<PhoneNavButton goto="app/collections" sprite="group" label="Collections" />
+	<PhoneNavButton
+		goto="/collections"
+		sprite="check"
+		label={activeCollectionProps.label}
+		isEnabled={activeCollectionProps.isEnabled}
+	/>
+	<PhoneNavButton goto="app/settings" sprite="settings" label="Settings" />
+</nav>
