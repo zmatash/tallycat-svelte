fe5de4ea49e84508c5ee805065f30c14061727a3
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-27 23:49:11 +0000

    card button to create new collections.

---
+++ src/lib/components/collection-card/create-collection-card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import getSprite from "$lib/utility/sprite-helpers";
+</script>
+
+<button aria-label="Create Collection" type="button" class="col-centred just-center gap-8">
+	<div class="icon-container col-centred just-center">
+		<svg>
+			<title>Plus</title>
+			<use href={getSprite("plus")} />
+		</svg>
+	</div>
+</button>
+
+<style>
+	button {
+		border-radius: var(--size16);
+		background: var(--surface0);
+		width: 100%;
+		aspect-ratio: 1.2;
+	}
+
+	svg {
+		width: var(--size40);
+		height: var(--size40);
+	}
+
+	.icon-container {
+		background: var(--surface1);
+		border-radius: 50%;
+		width: var(--size64);
+		height: var(--size64);
+	}
+</style>
