0b27bfe512c76545dcae3e172acaa5b1cf36b1f0
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-28 22:21:37 +0000

    add base components for card based components.

---
+++ src/lib/components/card-button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { WithChildren } from "$lib/utility/svelte/types";
+
+	interface Props extends WithChildren {
+		class?: string;
+		buttonType: "button" | "submit" | "reset";
+		ariaLabel: string;
+		onClick?: () => void;
+	}
+
+	let props: Props = $props();
+</script>
+
+<button class={`card ${props.class}`} type={props.buttonType} aria-label={props.ariaLabel} onclick={props.onClick}>
+	{@render props.children()}
+</button>
+
+<style>
+	.card {
+		border-radius: var(--size16);
+		background: var(--surface0);
+	}
+</style>
---
+++ src/lib/components/card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { WithChildren } from "$lib/utility/svelte/types";
+
+	interface Props extends WithChildren {
+		class?: string;
+	}
+
+	let props: Props = $props();
+</script>
+
+<div class={`card ${props.class}`}>
+	{@render props.children()}
+</div>
+
+<style>
+	.card {
+		border-radius: var(--size16);
+		background: var(--surface0);
+	}
+</style>
