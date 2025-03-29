58565e520e6c5c197bdcb5c2c4fd8cdff3565992
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-29 22:37:42 +0000

    add counter state.

---
+++ src/lib/utility/state/counter.ts
@@ -0,0 +1,53 @@
+import type { CounterRow, PartialWithId } from "$lib/common/types";
+
+export interface CounterState {
+	value: number;
+	id: number;
+	name: string;
+	index: number;
+	collectionId: number;
+}
+
+function toRow(counter: CounterState): CounterRow {
+	return {
+		id: counter.id,
+		name: counter.name,
+		value: counter.value,
+		position: counter.index,
+		collection_id: counter.collectionId
+	};
+}
+
+function toRowPartial(counter: PartialWithId<CounterState>): PartialWithId<CounterRow> {
+	return {
+		id: counter.id,
+		name: counter.name,
+		value: counter.value,
+		position: counter.index
+	};
+}
+
+function toRowOmitId(counter: Omit<CounterState, "id">): Omit<CounterRow, "id" | "collection_id"> {
+	return {
+		name: counter.name,
+		value: counter.value,
+		position: counter.index
+	};
+}
+
+function fromRow(counter: CounterRow): CounterState {
+	return {
+		id: counter.id,
+		name: counter.name,
+		value: counter.value,
+		index: counter.position,
+		collectionId: counter.collection_id
+	};
+}
+
+export const counterState = {
+	toRow,
+	toRowPartial,
+	toRowOmitId,
+	fromRow
+} as const;
