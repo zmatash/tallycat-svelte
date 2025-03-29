11708b1e1faf79182052dbb24474a12dea94497d
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-29 11:28:47 +0000

    add selection state with highlight to base card.

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -3,12 +3,13 @@
 
 	interface Props extends WithChildren {
 		class?: string;
+		selected?: boolean;
 	}
 
 	let props: Props = $props();
 </script>
 
-<div class={`card ${props.class}`}>
+<div class={[`row-centred gap-16 pad-16 card ${props.class}`, { active: props.selected === true }]}>
 	{@render props.children()}
 </div>
 
@@ -16,5 +17,10 @@
 	.card {
 		border-radius: var(--size16);
 		background: var(--surface0);
+		border: 3px solid transparent;
+	}
+
+	.active {
+		border: 3px solid var(--blue);
 	}
 </style>
