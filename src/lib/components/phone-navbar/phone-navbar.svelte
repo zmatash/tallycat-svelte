5cb38581303525379196be86faa04c2649cf883a
Matt Ashpole <m.d.ashpole@gmail.com>
2025-03-29 11:44:25 +0000

    simplify navbar condition.

--- src/lib/components/phone-navbar/phone-navbar.svelte
+++ src/lib/components/phone-navbar/phone-navbar.svelte
@@ -4,21 +4,19 @@
 	import PhoneNavButton from "./phone-nav-button.svelte";
 
 	let activeCollectionProps = $derived.by(() => {
-		if (!profileStore.profile)
-			return {
-				label: "Disabled",
-				isEnabled: false
-			};
 		const activeId = profileStore.profile?.activeCollection;
-		if (!activeId)
+		const collection = activeId ? collectionStore.getCollection(activeId) : null;
+
+		if (!profileStore.profile || !activeId || !collection) {
 			return {
 				label: "Disabled",
 				isEnabled: false
 			};
-		const collection = collectionStore.collections.find((c) => c.id === activeId);
+		}
 		return {
-			label: collection?.name ?? "Disabled",
-			isEnabled: collection !== undefined
+			label: collection.name,
+			isEnabled: true,
+			goto: `/app/collections/${collection.id}`
 		};
 	});
 </script>
